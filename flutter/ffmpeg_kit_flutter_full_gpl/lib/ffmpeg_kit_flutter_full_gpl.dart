d83c6d3fb472b463603fa9b47f8eeb868089b02c
mhikaru0131 <main.hikaru0131@gmail.com>
2025-03-31 23:24:36 +0900

    Update ffmpeg_kit_flutter_full_gpl.dart

--- flutter/ffmpeg_kit_flutter_full_gpl/lib/ffmpeg_kit_flutter_full_gpl.dart
+++ flutter/ffmpeg_kit_flutter_full_gpl/lib/ffmpeg_kit_flutter_full_gpl.dart
@@ -1,1 +1,4 @@
 library ffmpeg_kit_flutter_full_gpl;
+export 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+export 'package:ffmpeg_kit_flutter/return_code.dart';
+export 'package:ffmpeg_kit_flutter/ffmpeg_session.dart';
